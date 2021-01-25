ef91eeae2d750888a4571222fba6e60ba5b61fa1
TheEpicBlock <git.teb@theepicblock.nl>
2021-01-25 12:21:01 +0100

    Init repo

---
+++ src/main/java/nl/theepicblock/polyprioritize/PolyPrioritize.java
@@ -0,0 +1,10 @@
+package nl.theepicblock.polyprioritize;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PolyPrioritize implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
